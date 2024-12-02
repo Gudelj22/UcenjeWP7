ebba1703052443e61400cbdfd74923301c6e512f
Gudelj22 <gudelj.petar2005@gmail.com>
2024-12-02 19:48:10 +0100

    Create SQLQuery1.sql

---
+++ SQLQuery1.sql
@@ -0,0 +1,24 @@
+use master;
+go
+drop database if exists PROBA;
+go
+create database PROBA;
+go
+use PROBA;
+go
+
+create table IspitniRok(
+sifra int not null primary key identity(1,1),
+predmet int not null,
+vrstaIspita int null,
+datu datetime not null,
+pristupio varchar not null
+);
+
+create table Pristupnici(
+ispitniRok datetime not null references ispitniRok(sifra),
+student int not null,
+brojBodova varchar(50) null,
+ocjena varchar(50) not null
+
+);
