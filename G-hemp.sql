9f0bcaab668e1b4c82b52fea147e9165fa840362
Gudelj22 <petargudelj96@gmail.com>
2024-12-22 14:56:13 +0100

    Update G-hemp.sql

--- G-hemp.sql
+++ G-hemp.sql
@@ -14,28 +14,35 @@ oib char(11) null,
 datum_rodenja datetime not null
 );
 
-create table narudzbe(
-sifra int not null primary key identity (1,1),
-kupac_namalo int not null references kupci(sifra),
-kupac_naveliko int not null references kupci(sifra)
-);
-
 create table robe(
 sifra int not null primary key identity (1,1),
 ime varchar(50) not null,
 vrsta varchar(50) not null,
 cijena decimal(10,2),
-narudzba int null references narudzbe(sifra)
+kolicina int not null,
+brojnarudzba int not null references narudzbe(sifra)
+
 );
 
-insert into kupci
-(ime,prezime,datum_rodenja) values
-('petar','gudelj','13-08-2005');
+create table narudzbe(
+sifra int not null primary key identity (1,1),
+kolicina int  null,
+cijena decimal(10,2) null ,
+roba int not null references robe(sifra),
+kupac int not null references kupci(sifra)
+);
 
-insert into narudzbe
-(kupac_namalo,kupac_naveliko) values
-('malo',1,'veliko',2);
+
+
+insert into kupci 
+(ime,prezime,datum_rodenja) values
+('petar','gudelj','13-08-2005',1);
 
 insert into robe
-(ime,vrsta,cijena) values
-('cbd','orangehase','20,59',1);
+(ime,vrsta,cijena,kolicina) values
+('cbd','orangehase','20,59','1',1);
+
+insert into narudzbe
+( kupac, roba) values
+(1,1);
+
